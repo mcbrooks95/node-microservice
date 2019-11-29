4cbacbbad3875f9b382a8b5fc6b2436a78fae383
Mark Brooks <mcbrooks95@gmail.com>
2019-11-28 21:21:36 -0600

    had issues with postgresql database

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // connectionString: "postgres://sfpbzylfgkxmwn:1c05515cd61b6f52018402d488250ce850be16a115c401af8988f6e40005b89b@localhost:5432/d31sdj5qhk8k8s",
+  // ssl: false
   ssl: true
 });
 const PORT = process.env.PORT || 5000
@@ -18,6 +20,9 @@ express()
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
+      console.log(`about to print results`)
+      console.log(results)
+      console.log(result)
       res.render('pages/db', results );
       client.release();
     } catch (err) {
