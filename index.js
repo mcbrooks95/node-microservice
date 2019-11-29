78991e77f9db5466f3cc567c9da6a74a65269895
Mark Brooks <mcbrooks95@gmail.com>
2019-11-28 18:31:48 -0600

    added remote db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,6 +13,18 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
