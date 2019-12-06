458a0815785ecaf70ab07d50103bdc95a5a8fa3c
Mark Brooks <mcbrooks95@gmail.com>
2019-12-06 16:16:49 -0600

    added cote test

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const mongoose = require('mongoose');
 var orders = require('./orders/orders');
 var customers = require('./customers/customers');
 var books = require('./books/books');
+const cote = require('cote');
 
 mongoose.connect("mongodb+srv://testuser:testpassword@cluster0-fnurh.mongodb.net/test", () => {
   console.log("database is connected!");
 })
+
+const bloodResponder = new cote.Responder({ name: 'blood Responder'})
+const patientRequester = new cote.Requester({ name: 'Patient Requester'})
+const blood = [
+  {
+    blood_type: "A+",
+    count: 10
+  },
+  {
+    blood_type: "A-",
+    count: 15
+  },
+  {
+    blood_type: "AB+",
+    count: 5
+  }
+]
+
+bloodResponder.on("list", req => {
+  return Promise.resolve(blood);
+})
+
 
 const PORT = process.env.PORT || 5000
 
@@ -18,6 +41,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get("/patients", async function(req, res) {
+    const patients = await patientRequester.send({ type: "list"})
+    res.json(patients).status(200)
+  })
   .use('/', orders)
   .use('/', customers)
   .use('/', books)
