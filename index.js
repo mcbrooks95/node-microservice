81c17e06a4e70d6879ee63851d15b307ec219582
Mark Brooks <mcbrooks95@gmail.com>
2019-12-06 12:51:06 -0600

    clean up

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ mongoose.connect("mongodb+srv://testuser:testpassword@cluster0-fnurh.mongodb.net
 
 const PORT = process.env.PORT || 5000
 
-// require("./Book")
-// const Book = mongoose.model("Book");
-
-
 express()
-  // .use(customers)
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
@@ -27,51 +22,6 @@ express()
   .use('/', orders)
   .use('/', customers)
   .use('/', books)
-  // .delete('/book/:id', (req, res) => { 
-  //   Book.findOneAndRemove(req.params.id).then(() => {
-  //       res.send("Book has been successfully removed!")
-  //   })
-  // })
-  // .get('/books', (req, res) => { 
-  //   Book.find().then((books) => {
-  //     res.json(books);
-  //   }).catch((err) => {
-  //     if(err) {
-  //       throw err;
-  //     }
-  //   })
-  // })
-  // .get('/book/:id', (req, res) => { 
-  //   Book.findById(req.params.id).then((book) => {
-  //       if(book) {
-  //         res.json(book);
-  //       }
-  //       else
-  //       {
-  //         res.sendStatus(404);
-  //       }
-  //   }).catch((err) => {
-  //       if(err) {
-  //         throw err;
-  //       }
-  //   })
-  // })
-  // .post('/book', (req, res) => { 
-  //     var newBook = {
-  //       title: req.body.title,
-  //       author: req.body.author,
-  //       numberPages: req.body.numberPages,
-  //       publisher: req.body.publisher
-  //     }
-
-  //     var book = new Book(newBook);
-
-  //     book.save().then(() => {
-  //       console.log("new book created")
-  //     })
-
-  //     res.send("a new book created with success!")
-  // })
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
