25504072cdf2218648ef973f5ab788f6ef551c8b
Mark Brooks <mcbrooks95@gmail.com>
2019-12-07 15:28:21 -0600

    clean up

--- orders/Order.js
+++ orders/Order.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 
-// var Schema = mongoose.Schema;
-// CustomerID = Schema.objectId;
-
 mongoose.model("Order", {
 
     CustomerID: {
         type: String,
-        // type: mongoose.Schema.objectId,
         required: true
     },
     BookID: {
         type: String,
-        // type: mongoose.SchemaTypes.BookID,
         required: true
     },
     initialDate: {
