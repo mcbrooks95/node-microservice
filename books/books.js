9f86e45d16189e0b623f8f94518a684c7f256427
Mark Brooks <mcbrooks95@gmail.com>
2019-12-21 17:09:18 -0600

    added it back

--- books/books.js
+++ books/books.js
@@ -2,49 +2,35 @@ const express = require("express")
 const app = express.Router();
 const bodyParser = require("body-parser");
 require("./Book")
-// const bookCote = require('./bookCote.js');
-const mongoose = require("mongoose");
-const Book = mongoose.model("Book");
+const bookCote = require('./bookCote.js');
 
 app.use(bodyParser.json());
 
-
-app.get('/books', (req, res) => {
-    Book.find().then((books) => {
-        res.json(books).status(200);
-    }).catch((err) => {
-        if(err) {
-            throw err;            
-        }
-        return null;
+app.post('/book', (req, res) => {
+    bookCote.Requester.send({ type: "bookpost", body: req.body})
+    .then((book) => {
+        res.json(book).status(200);
     })
 })
 
-// app.get('/books', (req, res) => {
-//     bookCote.Requester.send({ type: "booklist"}).then((books) => {
-//         res.json(books).status(200);
-//     })
-// })
+app.get('/book/:id', (req, res) => { 
+    bookCote.Requester.send({ type: "bookget", body: req.params.id})
+    .then((book) => {
+        res.json(book).status(200);
+    })
+})
 
-// app.post('/book', (req, res) => {
-//     bookCote.Requester.send({ type: "bookpost", body: req.body})
-//     .then((book) => {
-//         res.json(book).status(200);
-//     })
-// })
+app.get('/books', (req, res) => {
+    bookCote.Requester.send({ type: "booklist"}).then((books) => {
+        res.json(books).status(200);
+    })
+  })
 
-// app.get('/book/:id', (req, res) => { 
-//     bookCote.Requester.send({ type: "bookget", body: req.params.id})
-//     .then((book) => {
-//         res.json(book).status(200);
-//     })
-// })
-
-// app.delete('/book/:id', (req, res) => {
-//     bookCote.Requester.send({ type: "bookdelete", id: req.params.id})
-//     .then(() => {
-//         res.send("Book has been successfully removed!");
-//     })
-// })
+app.delete('/book/:id', (req, res) => {
+    bookCote.Requester.send({ type: "bookdelete", id: req.params.id})
+    .then(() => {
+        res.send("Book has been successfully removed!");
+    })
+})
 
 module.exports = app;
