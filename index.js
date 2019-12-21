b14bd757e255c5c8a6d8959bdf85cc6a80234072
Mark Brooks <mcbrooks95@gmail.com>
2019-12-21 16:55:45 -0600

    testing to see if I can get heroku to work again

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require(`body-parser`);
 const mongoose = require('mongoose');
-var orders = require('./orders/orders');
-var customers = require('./customers/customers');
+// var orders = require('./orders/orders');
+// var customers = require('./customers/customers');
 var books = require('./books/books');
 
 mongoose.connect("mongodb+srv://testuser:testpassword@cluster0-fnurh.mongodb.net/test", () => {
@@ -17,7 +17,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .use('/', orders)
-  .use('/', customers)
+  // .use('/', orders)
+  // .use('/', customers)
   .use('/', books)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
